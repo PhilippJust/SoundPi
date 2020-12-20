a960a14231ea2991b996473fdb7f936fa92e668d
Philipp Just <p.just@fm-maschinenbau.de>
2020-12-20 17:11:09 +0100

    Print on missing host

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
-#!/bin/bash -eu
+#!/bin/bash
+host=$1
+
+if [ -z "$host" ]; then
+    echo "Please provide a host like 'deploy 127.0.0.1'"
+    exit 1
+fi
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook --inventory ${1}, \
+ansible-playbook --inventory ${host}, \
     --user pi --ask-pass \
     --extra-vars @my_config.yml \
     soundpi.yml
